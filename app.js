1b1cd1dbcd1754cd5f794214234efd49ec43b81e
manishSanodiya <122765253+manishSanodiya@users.noreply.github.com>
2023-09-18 19:07:54 +0530

    two

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-const http = require('http');
+const http = require("http");
 
-const express = require('express');
-console.log('routes.someText');
+const express = require("express");
+
+//import body parser after (npm install --save body-parser)
+const bodyParser = require("body-parser");
+
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log("in the middleware")
-    next()
-})
-app.use((req,res,next)=>{
-    console.log("in the new middleware")
-    res.send('<h1> hello to node js </h1>')
-  
-})
+//body-parser is used before any routing middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/addProducts", (req, res, next) => {
+  res.send(
+    '<form action="/products" method="POST" ><input type="text" name="title"/><input type="text" name="disc"/><button type="submit">Add Products</button></form>'
+  );
+});
+
+app.post("/products", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1> hello to node js </h1>");
+});
 
 const servers = http.createServer(app);
 
-servers.listen(4000)
+servers.listen(4000);
